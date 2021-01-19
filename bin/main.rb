598f393bcf9f49898787b43b3a63d730465c7ca1
atenaiis <attennaisc@gmail.com>
2021-01-19 17:20:21 -0600

    adden .env to gitignore

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,7 @@ require 'work'
         client.search('#chocolate', result_type: "recent").take(3).each do |tweet|
         client.update  ("#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? ")
       client.fav tweet    
-      end
+    
 
      rescue
           puts 'error occurred, waiting for 5 seconds'
--- lib/work.rb
+++ lib/work.rb
@@ -1,17 +1,16 @@
 require 'twitter'
-require 'dotenv'
-Dotenv.load
+
 
 loop do
   begin
    client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV['CONSUMER_KEY']
-    config.consumer_secret = ENV['CONSUMER_SECRET']
-    config.access_token_key = ENV['ACCESS_TOKEN']
-    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    config.consumer_key        =  "xXG6cIycqpZauOTPutodCC3fw"
+    config.consumer_secret     = "im7i6GcqxvLAQhKKVXEoeLUWut9TdG0ZSVYzl39k5wqql3C61O"
+    config.access_token        = "744842330050465793-vltnR6Jp9T4dHvrourG2mNsRY0C8yEH"
+    config.access_token_secret = "4V3LFjBj98qDHTpxWel3hIvb9XxRHoU6Vu7i2MFyqwXva"
    end
 
-   client.search('#chocolate', result_type: 'recent').take(3).each do |tweet|
+   client.search('#chocolate', result_type: 'recent').take(5).each do |tweet|
      client.update "#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? "
      client.fav tweet
    end
