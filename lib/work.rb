4b5cc66ffc14426c4529584ad5126aa3cffc608b
atenaiis <attennaisc@gmail.com>
2021-01-20 08:16:55 -0600

    added rubocop

--- lib/work.rb
+++ lib/work.rb
@@ -1,13 +1,14 @@
 require 'twitter'
-
+require 'dotenv'
+Dotenv.load
 
 loop do
   begin
    client = Twitter::REST::Client.new do |config|
-    config.consumer_key        =  "xXG6cIycqpZauOTPutodCC3fw"
-    config.consumer_secret     = "im7i6GcqxvLAQhKKVXEoeLUWut9TdG0ZSVYzl39k5wqql3C61O"
-    config.access_token        = "744842330050465793-vltnR6Jp9T4dHvrourG2mNsRY0C8yEH"
-    config.access_token_secret = "4V3LFjBj98qDHTpxWel3hIvb9XxRHoU6Vu7i2MFyqwXva"
+    config.consumer_key        = ENV['CONSUMER_KEY']
+    config.consumer_secret     = ENV['CONSUMER_SECRET']
+    config.access_token        = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
    end
 
    client.search('#chocolate', result_type: 'recent').take(5).each do |tweet|
