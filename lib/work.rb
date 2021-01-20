fe56856f26e543b13fbcc93f7bdada344b821832
atenaiis <attennaisc@gmail.com>
2021-01-20 08:36:10 -0600

    added rspec

--- lib/work.rb
+++ lib/work.rb
@@ -5,9 +5,9 @@ Dotenv.load
 loop do
   begin
    client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['CONSUMER_KEY']
-    config.consumer_secret     = ENV['CONSUMER_SECRET']
-    config.access_token        = ENV['ACCESS_TOKEN']
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token = ENV['ACCESS_TOKEN']
     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
    end
 
@@ -15,7 +15,6 @@ loop do
      client.update "#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? "
      client.fav tweet
    end
- rescue 
    puts 'error occurred, waiting for 5 seconds'
    sleep 5
  end
