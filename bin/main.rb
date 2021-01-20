3bb647e74d1ac8ff1fa2cba32f425747a4952125
atenaiis <attennaisc@gmail.com>
2021-01-20 13:00:04 -0600

    update readme file

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,4 @@
-require'twitter'
-require 'work'
+require_relative '../lib/work'
        
-        client.search('#chocolate', result_type: "recent").take(3).each do |tweet|
-        client.update  ("#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? ")
-      client.fav tweet    
+client.new 
     
-
-     rescue
-          puts 'error occurred, waiting for 5 seconds'
-          sleep 5
-       end
-     end
-    end
--- lib/work.rb
+++ lib/work.rb
@@ -5,10 +5,10 @@ Dotenv.load
 loop do
   begin
    client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV['CONSUMER_KEY']
-    config.consumer_secret = ENV['CONSUMER_SECRET']
-    config.access_token = ENV['ACCESS_TOKEN']
-    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+     config.consumer_key = ENV['CONSUMER_KEY']
+     config.consumer_secret = ENV['CONSUMER_SECRET']
+     config.access_token = ENV['ACCESS_TOKEN']
+     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
    end
 
    client.search('#chocolate', result_type: 'recent').take(5).each do |tweet|
