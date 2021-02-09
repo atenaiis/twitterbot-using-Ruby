b8f09dbfcd7758b0a1f7c123f978f44339c2516a
atenaiis <attennaisc@gmail.com>
2021-02-09 14:43:45 -0600

    added new function, twitts one birth event from Wikipedia, On this Day

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,21 @@
-require_relative '../lib/work'
+require 'twitter'
+require_relative '../lib/credentials.rb'
+require_relative '../lib/work.rb'
+require_relative '../lib/json_info.rb'
+require_relative '../lib/twitter_scrip.rb'
 
-client.new
+client = Chocolate.new
+bot = TwitterBot.new
+
+loop do
+  time = Time.new
+  events_string_daily = JsonData.new('births', time.day, time.month)
+  daily_tweet = Tweet.new(events_string_daily.json_hash)
+
+  5.times do
+    content = (1..290).to_a
+    content = daily_tweet.tweet_content until content.length < 280
+    bot.tweet(content)
+    
+  end
+end
--- lib/work.rb
+++ lib/work.rb
@@ -1,19 +1,17 @@
 require 'twitter'
 require 'dotenv'
 Dotenv.load
+class Chocolate
 
-loop do
   client = Twitter::REST::Client.new do |config|
     config.consumer_key = ENV['CONSUMER_KEY']
     config.consumer_secret = ENV['CONSUMER_SECRET']
     config.access_token = ENV['ACCESS_TOKEN']
     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
   end
-
+  5.times do
   client.search('#chocolate', result_type: 'recent').take(5).each do |tweet|
     client.update "#{tweet.user.screen_name}: #{tweet.text}:Hey I love chocolate too, what are your favorite blogs? "
-    client.fav tweet
-  end
-  puts 'error occurred, waiting for 5 seconds'
-  sleep 5
+end
+end
 end
